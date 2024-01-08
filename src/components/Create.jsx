46d324f8ad64e1667f20564e1611d54fc0bc7887
Menahasrinivas <menahasrinivas@gmail.com>
2024-01-08 23:00:42 +0530

    commit

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,97 +1,120 @@
-import React,{ useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Topbar from './Topbar';
-import AxiosCards from './Common/AxiosCards';
-import axios from 'axios';
-import{ API_URL } from '../App';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Topbar from "./Topbar";
+import AxiosCards from "./Common/AxiosCards";
+import axios from "axios";
+import { API_URL } from "../App";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Create() {
-              let [name,setName] = useState("")
-              let[address,setAdrs] = useState("")
-              let[phonenumber,setPhn] = useState("")
-              let [email,seteMail] = useState("")
-              let [companyname,setCompany] = useState("")
-              let[image,setImage] = useState("")
-              let navigate = useNavigate()
-          const handleCreate = async()=>{
-          try {
-              let data ={name,address,phonenumber,email,companyname,image,status:false};
-         let res = await axios.post(API_URL, data);
-          if(res.status === 201){
+  let [name, setName] = useState("");
+  let [address, setAddress] = useState("");
+  let [phone, setPhone] = useState("");
+  let [email, setEmail] = useState("");
+  let [company, setCompany] = useState("");
+  let [image, setImage] = useState("");
+  let navigate = useNavigate();
 
-              toast.success("Data Created Successfully!")
-              navigate('/dashboard')
-}
+  const handleCreate = async () => {
+    try {
+      let data = { name, address, phone, email, company, image, status: true };
+      let res = await axios.post(API_URL, data);
+      if (res.status === 201) {
+        toast.success("Data Created Successfully!");
+        navigate("/dashboard");
+    }
+     } catch (error) {
+     };
+    }
+  return 
+    <div className='container-fluid'>
+      <Topbar />
+      <div className="homeWrapper">
+        <div className="formWrapper">
+          <Form >
+            <Form.Group className="mb-3">
+              <Form.Lable>Name</Form.Lable>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) => {
+                  setName(e.target.value)}}/>
+            </Form.Group>
 
-} catch (error){
+            <Form.Group className="mb-3">
+              <Form.Lable>Address</Form.Lable>
+              <Form.Control
+                type="text"
+                placeholder="Address"
+                onChange={(e) => {
+                  setAddress(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Lable>Phone</Form.Lable>
+              <Form.Control
+                type="text"
+                placeholder="Phonenumber"
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+              />
+            </Form.Group>
 
-}
+            <Form.Group className="mb-3">
+              <Form.Lable>Emailail</Form.Lable>
+              <Form.Control
+                type="text"
+                placeholder="Email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Lable>Company</Form.Lable>
+              <Form.Control
+                type="text"
+                placeholder="company"
+                onChange={(e) => {
+                  setCompany(e.target.value);
+                }}
+              />
+            </Form.Group>
 
-};    
-  
-  
- return  <div className='container-fluid'>
-         
-<Topbar/>
-<div className='home'>
-      <div className='formWrapper'>
- 
-              <Form>
-                            <Form.Group className="mb-3">
-                            <Form.Lable>Name</Form.Lable>
-                            <Form.Control type="text" placeholder="Name" onChange={(e)=>{setName(e.target.value)}}/>
-                            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Image Url</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Image Url"
+                onChange={(e) => {
+                  setImage(e.target.value);
+                }}
+              />
+            </Form.Group>
 
-                            <Form.Group className="mb-3">
-                            <Form.Lable>Address</Form.Lable>
-                            <Form.Control type="text" placeholder="Address" onChange={(e)=>{setAdrs(e.target.value)}}/>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                            <Form.Lable>Phonenumber</Form.Lable>
-                            <Form.Control type="text" placeholder="Phonenumber" onChange={(e)=>{setPhn(e.target.value)}}/>
-                            </Form.Group>
-                            
-                            <Form.Group className="mb-3">
-                            <Form.Lable>e-mail</Form.Lable>
-                            <Form.Control type="text" placeholder="email" onChange={(e)=>{seteMail(e.target.value)}}/>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                            <Form.Lable>CompanyName</Form.Lable>
-                            <Form.Control type="text" placeholder="companyname" onChange={(e)=>{setCompany(e.target.value)}}/>
-                            </Form.Group>
-                            
-
-                            <Form.Group className="mb-3">
-                             <Form.Label>Image Url</Form.Label>
-                             <Form.Control type="text" placeholder="Image Url" onChange={(e)=>{setImage(e.target.value)}}/>
-                             </Form.Group>
- 
-                       <Button variant="primary" onClick={()=>handleCreate()}>
-                            Submit
-                            </Button>
-                        </Form>
-         <div className='previewWrapper'>
-              <h2 style={{textAlign:"center"}}>Preview</h2>
-              <AxiosCards
+            <Button variant="primary" onClick={() => handleCreate()}>
+              Submit
+            </Button>
+          </Form>
+          </div>
+          <div className="previewWrapper">
+            <h2 style={{ textAlign: "center" }}>Preview</h2>
+            <AxiosCards
               name={name}
               address={address}
-              phonenumber={phonenumber}
+              phone={phone}
               email={email}
-              companyname={companyname}
-              image={image}
-               />
-              </div>             
-              </div>
-              </div>
-              </div>
- 
-
-
-
-
+              company={company}
+              image={image}/>
+          </div>
+        </div>
+      </div>
+    
+  
 }
 
-export default Create;
+export default Create
